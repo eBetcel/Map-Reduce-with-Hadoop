9f865d5f6f8988fb82a409b23cce04b6d9385658
Emanuel Betcel <emanuelbetcel@gmail.com>
2022-10-30 11:51:32 -0300

    update files

--- mapper.py
+++ mapper.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 import sys
+import csv
+
 # Read each line from stdin
-for line in sys.stdin:
+# for line in (sys.stdin.readlines()):
+for line in csv.reader(sys.stdin.readlines(), delimiter=','):
   # Get the words in each line
-  words = line.split()
-  # Generate the count for each word
-  for word in words:
-    # Write the key-value pair to stdout to be processed by
-    # the reducer.
-    # The key is anything before the first tab character and the
-    # value is anything after the first tab character.
-    print ('{0}\t{1}'.format(word, 1))
+  print(line)
