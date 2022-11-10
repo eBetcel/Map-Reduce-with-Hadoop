5f04a27d69bd972a0d5da840c9291a5d1fc069ca
Emanuel Betcel <emanuelbetcel@gmail.com>
2022-11-10 00:52:15 -0300

    finished mapper and started reducer

--- mapper.py
+++ mapper.py
@@ -4,6 +4,15 @@ import csv
 
 # Read each line from stdin
 # for line in (sys.stdin.readlines()):
-for line in csv.reader(sys.stdin.readlines(), delimiter=','):
-  # Get the words in each line
-  print(line)
+for line in csv.reader(sys.stdin.readlines(), delimiter=","):
+    # Get the words in each line
+    element = list(line)
+    data = (element[0], element[-2][1:])
+    salary = data[1]
+    name = data[0].split(",")
+    if len(name) == 2:
+        formated_name = name[1] + " " + name[0]
+    else:
+        formated_name = name[0]
+
+    print("{0}, {1}\t".format(formated_name, salary))
