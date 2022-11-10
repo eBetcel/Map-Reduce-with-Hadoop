e542aff57d3c897b88c32371f9aaec65bacb709b
Emanuel Betcel <emanuelbetcel@gmail.com>
2022-11-10 02:03:37 -0300

    formatted code with black

--- task_1/mapper.py
+++ task_1/mapper.py
@@ -5,11 +5,12 @@ import csv
 # Read each line from stdin
 # for line in (sys.stdin.readlines()):
 for line in csv.reader(sys.stdin.readlines(), delimiter=","):
-    # Get the words in each line
+    # Get each line and take names and salary
     element = list(line)
     data = (element[0], element[-2][1:])
     salary = data[1]
     name = data[0].split(",")
+    # Format name to First name + Last name style
     if len(name) == 2:
         formated_name = name[1] + " " + name[0]
     else:
